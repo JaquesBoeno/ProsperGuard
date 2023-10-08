1a8bc70f32b47970d7229343c2cf0da93571a00e
JaquesBoeno <jaquesboeno@proton.me>
2023-10-08 15:07:47 -0300

    add AsideBar

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
+import { SideBar } from './components/SideBar'
+import { Calcs } from './pages/Calcs'
+
 const App = () => {
-  return <p>hello world</p>
+  return (
+    <div className="AppContainer">
+      <BrowserRouter>
+        <SideBar />
+        <div className="pages container">
+          <Routes>
+            <Route path="/" element={<Calcs />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export { App }
---
+++ packages/web/src/pages/Calcs/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.scss'
+
+const Calcs: React.FC = () => {
+  return (
+    <div className="">
+      <h1></h1>
+    </div>
+  )
+}
+
+export { Calcs }
