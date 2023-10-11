dd967a2151c31520923271b468773f4ae2b3ba97
JaquesBoeno <jaquesboeno@proton.me>
2023-10-11 20:46:50 -0300

    currency input use Imask now

--- packages/web/src/components/Input/CurrencyInput.tsx
+++ packages/web/src/components/Input/CurrencyInput.tsx
@@ -1,52 +1,30 @@
-import React, { Dispatch, SetStateAction, useState } from 'react'
+import React from 'react'
 
 import './styles.scss'
+import { useIMask } from 'react-imask'
 import { StandardInput } from '.'
 
 interface Props extends React.HTMLProps<HTMLInputElement> {
   label: string
-  setOutputValue?: Dispatch<SetStateAction<number | undefined>>
 }
 
-const CurrencyInput: React.FC<Props> = ({ label, setOutputValue, ...rest }) => {
-  const [value, setValue] = useState('')
+const CurrencyInput: React.FC<Props> = ({ label, ...rest }) => {
+  const { ref } = useIMask({
+    mask: 'R$ num',
+    lazy: false,
+    blocks: {
+      num: {
+        mask: Number,
+        scale: 2,
+        thousandsSeparator: '.',
+        padFractionalZeros: true,
+        radix: ',',
+        mapToRadix: ['.'],
+      },
+    },
+  })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let str = e.target.value
-    let [integer, decimal] = str
-      .replace(/\D/g, '')
-      .padStart(4, '0')
-      .split(/(?=..$)/)
-
-    integer = Number(integer).toLocaleString('pt-br')
-
-    str = [integer, decimal].join(',').replace(/^0+/, '').padStart(4, '0')
-
-    if (str == '0.00') str = ''
-
-    setValue(str)
-
-    if (setOutputValue)
-      setOutputValue(
-        Number(
-          str
-            .replace(/[,.]/g, '')
-            .split(/(?=..$)/)
-            .join('.')
-        )
-      )
-  }
-
-  return (
-    <StandardInput
-      label={label}
-      value={value}
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}
-      className="CurrencyInput"
-      prefix="R$"
-      {...rest}
-    />
-  )
+  return <StandardInput reference={ref} label={label} {...rest} />
 }
 
 export { CurrencyInput }
