5cd0dc4360af8250aa07815c3786ab311ff422e0
JaquesBoeno <jaquesboeno@proton.me>
2023-10-13 11:40:40 -0300

    add Equivalence Of fees calculator

---
+++ packages/web/src/components/EquivalenceOfFees/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { EquivalenceOfFees as formula } from '../../utils/formulasFinancialMathematics'
+
+import './styles.scss'
+import { Input } from '../Input'
+import { SelectInput } from '../Input/SelectInput'
+import { Button } from '../Button'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const EquivalenceOfFeesSchema = z.object({
+  interest: z.string().transform((value) => {
+    return Number(value.replace(/[^0-9,]/g, '').replace(',', '.')) / 100
+  }),
+  to: z.enum(['yearlyToMonthly', 'monthlyToYearly']),
+})
+
+type EquivalenceOfFessFormData = z.infer<typeof EquivalenceOfFeesSchema>
+
+const EquivalenceOfFees: React.FC = () => {
+  const { register, handleSubmit } = useForm<EquivalenceOfFessFormData>({
+    resolver: zodResolver(EquivalenceOfFeesSchema),
+  })
+
+  const [result, setResult] = useState<number>()
+
+  const onSubmit: SubmitHandler<EquivalenceOfFessFormData> = ({
+    interest,
+    to,
+  }) => {
+    setResult(formula(interest, to))
+  }
+
+  return (
+    <section className="EquivalenceOfFees">
+      <h1>Equivalência de Juros</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input type="percent" label="Juros" {...register('interest')} />
+        <SelectInput
+          label="Converter para..."
+          options={[
+            { name: 'De ano para mês', value: 'yearlyToMonthly' },
+            { name: 'De mês para ano', value: 'monthlyToYearly' },
+          ]}
+          {...register('to')}
+        />
+        <Button title="Converter" type="submit" />
+        <div>
+          <p>Taxa Convertida: {result && (result * 100).toFixed(2)} %</p>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export { EquivalenceOfFees }
--- packages/web/src/pages/Calcs/index.tsx
+++ packages/web/src/pages/Calcs/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './styles.scss'
 import { CompoundInterestCalculator } from '../../components/CompoundInterestCalculator'
+import { EquivalenceOfFees } from '../../components/EquivalenceOfFees'
 
 const Calcs: React.FC = () => {
   return (
     <div className="CalcsPage">
       <CompoundInterestCalculator />
+      <EquivalenceOfFees />
     </div>
   )
 }
