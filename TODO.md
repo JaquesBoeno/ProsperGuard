a261c4c730281bbcfd23086d1bb4e16d21a852c0
JaquesBoeno <jaquesboeno@proton.me>
2023-12-20 16:22:54 -0300

    improved sidebar

---
+++ TODO.md
@@ -0,0 +1,9 @@
+## Front-End
+
+- [ ] improve the layout
+- [ ] Create search transactions
+-
+
+## Back-End
+
+- [ ] init the server in golang
--- packages/web/src/components/SideBar/index.tsx
+++ packages/web/src/components/SideBar/index.tsx
@@ -1,20 +1,41 @@
 import React from 'react'
 
 import './styles.scss'
+import { MdLogout } from 'react-icons/md'
+import { LuHome, LuCalculator } from 'react-icons/lu'
+import { GrTransaction } from 'react-icons/gr'
 import { Link } from 'react-router-dom'
 
 const SideBar: React.FC = () => {
   return (
     <aside>
-      <h1>App logo</h1>
-      <ul>
-        <li>
-          <Link to="/">Calculadora</Link>
-        </li>
-        <li>
-          <Link to="/calcs">Calculadora</Link>
-        </li>
-      </ul>
+      <div className="TopBox">
+        <img src="/logos/imagotipo.svg" alt="Finance Manager" />
+        <ul>
+          <li>
+            <Link to="/">
+              <LuHome />
+              <span>Visão Geral</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/transactions">
+              <GrTransaction />
+              <span>Transações</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/calcs">
+              <LuCalculator />
+              <span>Calculadora</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <button className="Logout">
+        <MdLogout />
+        <span>Logout</span>
+      </button>
     </aside>
   )
 }
