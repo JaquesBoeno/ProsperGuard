c2c4ced10ad1db7949cd5a0b70d376054eec8ab4
JaquesBoeno <jaquesboeno@proton.me>
2024-01-28 16:40:54 -0300

    feat(back-end): add filter by value: min, max and exact

--- applications/server/internal/controller/transactionController.go
+++ applications/server/internal/controller/transactionController.go
@@ -83,6 +83,33 @@ func (t *TransactionController) GetAllTransactionsFromOneUser(c fiber.Ctx) error
 	if ok := typeFieldIsValid(typeFilter); typeFilter != "" && !ok {
 		return c.Status(fiber.StatusBadRequest).Send([]byte("Failed on create the transaction, provide a valid 'type' (income OR expense)"))
 	}
+	var value_min float64 = 0
+	var value_max float64 = 0
+	var value_exact float64 = 0
+
+	if m["value_min"] != "" {
+		value_min, err = strconv.ParseFloat(m["value_min"], 64)
+		if err != nil {
+			log.Println(fmt.Sprintf("TransactionController, CreateTransaction, format value: %v", err))
+			return c.Status(fiber.StatusInternalServerError).Send([]byte("Failed on create the transaction"))
+		}
+	}
+
+	if m["value_max"] != "" {
+		value_max, err = strconv.ParseFloat(m["value_max"], 64)
+		if err != nil {
+			log.Println(fmt.Sprintf("TransactionController, CreateTransaction, format value: %v", err))
+			return c.Status(fiber.StatusInternalServerError).Send([]byte("Failed on create the transaction"))
+		}
+	}
+
+	if m["value_exact"] != "" {
+		value_exact, err = strconv.ParseFloat(m["value_exact"], 64)
+		if err != nil {
+			log.Println(fmt.Sprintf("TransactionController, CreateTransaction, format value: %v", err))
+			return c.Status(fiber.StatusInternalServerError).Send([]byte("Failed on create the transaction"))
+		}
+	}
 
 	transactions, err := user.QueryTransactions().
 		Where(func(s *sql.Selector) {
@@ -90,6 +117,15 @@ func (t *TransactionController) GetAllTransactionsFromOneUser(c fiber.Ctx) error
 				s.Where(sql.InValues("type", typeFilter))
 			}
 
+			if value_exact > 0 {
+				s.Where(sql.EQ("value", value_exact))
+			} else if value_min > 0 && value_max > value_min {
+				s.Where(sql.And(sql.GTE("value", value_min), sql.LTE("value", value_max)))
+			} else if value_min > 0 && value_max <= value_min {
+				s.Where(sql.GTE("value", value_min))
+			} else if value_min == 0 && value_max > value_min {
+				s.Where(sql.LTE("value", value_max))
+			}
 		}).
 		All(t.Ctx)
 
